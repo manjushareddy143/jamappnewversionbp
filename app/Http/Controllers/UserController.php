ef7de7fc0df2b8e486a14c0755495e65ec172787
jaydeep <jr@savitriya.com>
2020-03-06 18:35:23 +0530

    Createu user login and register api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -88,8 +88,45 @@ class UserController extends Controller
         return redirect()->route('Users.index')->with('Success','User deleted successfully');
     }
 
-    // User Login Function
+    // User Login API
 
+    /**
+     * @SWG\Post(
+     *   path="/login",
+     *   summary="User Login",
+     *     description="User will be logged in",
+     *   operationId="userLogin",
+     *   consumes={"application/xml","application/json"},
+     *   produces={"application/json"},
+     *     @SWG\Parameter(
+     *      in="body",
+     *      name="body",
+     *      description="Enter email address and password for user login",
+     *      required=true,
+     *     @SWG\Definition(
+     *         definition="users",
+     *         required={"username","passwrod"},
+     *         @SWG\Property(
+     *             description="Enter Email id",
+     *             property="username",
+     *             type="string"
+     *         ),
+     *         @SWG\Property(
+     *             description="Enter User Password",
+     *             property="password",
+     *             type="string"
+     *         ),
+     *       )
+     *      ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="User logged in Successfully!"
+     *   ),
+     *   @SWG\Response(response=404, description="Page not Found"),
+     *   @SWG\Response(response=500, description="internal server error"),
+     * )
+     *
+     */
     public function login(Request $request) {
         try {
             $response = array();
@@ -118,7 +155,77 @@ class UserController extends Controller
             $response['message'] = "There is some error";
         }
     }
+    // User Login API
 
-    // User Login Function
 
+    // User Register API
+    /**
+     * @SWG\Get(
+     *   path="/register",
+     *   summary="User Register",
+     *     description="User will be logged in",
+     *   operationId="userLogin",
+     *   consumes={"application/xml","application/json"},
+     *   produces={"application/json"},
+     *     @SWG\Parameter(
+     *      in="body",
+     *      name="body",
+     *      description="Enter username, first_name, last_name, email address, mobile number and password for user Register",
+     *      required=true,
+     *     @SWG\Definition(
+     *         definition="users",
+     *         required={"name","email","password",confirm_password},
+     *         @SWG\Property(
+     *             description="Enter name",
+     *             property="name",
+     *             type="string"
+     *         ),
+     *
+     *     @SWG\Property(
+     *             description="Enter email address",
+     *             property="email",
+     *             type="string"
+     *         ),
+     *         @SWG\Property(
+     *             description="Enter User Password",
+     *             property="password",
+     *             type="string"
+     *         ),
+     *     @SWG\Property(
+     *             description="Enter User Password",
+     *             property="confirm_password",
+     *             type="string"
+     *         ),
+     *       )
+     *      ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="User Register  Successfully!"
+     *   ),
+     *   @SWG\Response(response=404, description="Page not Found"),
+     *   @SWG\Response(response=500, description="internal server error"),
+     * )
+     *
+     */
+
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+                'confirm_password' => 'required|same:password',
+            ]);
+        if ($validator->fails())
+        {
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('AppName')->accessToken;
+        return response()->json(['success'=>$success], $this->successStatus);
+
+        }
 }
