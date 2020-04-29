d744bbf89e434e330ab012d35ecda9e943c910df
Mayur <mk@savitriya.com>
2020-04-29 12:27:58 +0530

    update css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="img/logo/logo.png" rel="icon">
-    <title>JAM - Register</title>
-   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="css/ruang-admin.css" rel="stylesheet" type="text/css">
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       <meta name="description" content="">
+       <meta name="author" content="">
+       <link href="img/logo/logo.png" rel="icon">
+       <title>JAM - Register</title>
+       <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+       <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+       <link href="css/ruang-admin.css" rel="stylesheet" type="text/css">
 
-  </head>
+   </head>
+
   <body class="bg-gradient-login">
     <!-- Service Provider Sign-Up Content -->
     <div class="container-login service-provider-sign-up">
