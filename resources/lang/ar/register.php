b249d759876ea49dd2fb5ec389f184b66c94632b
jaydeep <jr@savitriya.com>
2020-05-30 15:00:49 +0530

    Apply multi language on Register page

---
+++ resources/lang/ar/register.php
@@ -0,0 +1,40 @@
+<?php
+
+return[
+
+'home_menu'=>'الصفحة الرئيسية',
+
+'about_menu'=>'رسالة',
+
+'Contact_menu'=>'رسالة',
+
+'message'=>'اشتراك مقدم الخدمة',
+
+'label_title'=>'للأفراد',
+
+'label_title2'=>'للتنظيم',
+
+'label_fname'=>'الاسم الاول',
+
+'label_lname'=>'الكنية',
+
+'label_mobile'=>'رقم الهاتف المحمول',
+
+'label_email'=>'عنوان بريد الكتروني',
+
+'label_password'=>'كلمه السر',
+
+'label_rstatus'=>'مركز المقيم',
+
+'label_btn'=>'سجل',
+
+'label_titlename'=>'شركة ملكية / شركة شراكة / شركة / مؤسسة تجارية',
+'label_cname'=>'اسم الشركة',
+'label_adname'=>'اسم المسؤول',
+'label_mnumber'=>'رقم الهاتف المحمول',
+'label_eaddress'=>'عنوان بريد الكتروني',
+'label_password2'=>'كلمه السر',
+'label_Country2'=>'بلد',
+
+
+];
---
+++ resources/lang/en/register.php
@@ -0,0 +1,42 @@
+<?php
+
+return[
+
+'home_menu'=>'Home',
+
+'about_menu'=>'About Us',
+
+'Contact_menu'=>'Contact US',
+
+'message'=>'Service Provider Sign-Up',
+
+'label_title'=>'FOR INDIVIDUAL',
+
+'label_title2'=>'FOR ORGANISATION',
+
+'label_fname'=>'First Name',
+
+'label_lname'=>'Last Name',
+
+'label_mobile'=>'Mobile Number',
+
+'label_email'=>'Email Address',
+
+'label_password'=>'Password',
+
+'label_rstatus'=>'Resident Status',
+
+'label_btn'=>'Sign Up',
+
+'label_cname'=>'Company Name',
+'label_adname'=>'Admin Name',
+'label_mnumber'=>'Mobile Number',
+'label_eaddress'=>'Email Address',
+'label_password2'=>'Password ',
+'label_Country2'=>'Country ',
+'label_titlename'=>'Proprietorship Firm / Partnership Firm / Company / Business Establishment ',
+
+
+ 
+];
+
