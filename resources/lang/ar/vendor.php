ff89db238c22dfc3d518910d794be4c61de5fb65
jaydeep <jr@savitriya.com>
2020-06-01 10:10:31 +0530

    aplly multi language for register and vendor page

---
+++ resources/lang/ar/vendor.php
@@ -0,0 +1,33 @@
+<?php
+
+return[
+
+'home_menu'=>'الصفحة الرئيسية',
+
+'about_menu'=>'رسالة',
+
+'Contact_menu'=>'رسالة',
+
+'label_title'=>'أضف البائعين',
+
+'label_fname'=>'الاسم الاول',
+
+'label_lname'=>'الكنية',
+
+'label_email'=>'عنوان بريد الكتروني',
+
+'label_password'=>'كلمه السر',
+
+'label_mobile'=>'رقم الهاتف المحمول',
+
+'label_gender'=>'جنس',
+
+'label_language'=>'انا اعرف اللغات',
+
+'label_Image'=>'صورة',
+
+'label_Country'=>'بلد',
+
+'label_Services'=>'خدمات',
+
+];
