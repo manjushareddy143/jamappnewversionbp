fe958de6182be3304e9c5bd880cfa2b43b5c90f3
jaydeep <jr@savitriya.com>
2020-06-19 16:15:10 +0530

    changes on sarvice mapping migration file

--- database/migrations/2020_04_13_072927_create_service_mappings_table.php
+++ database/migrations/2020_04_13_072927_create_service_mappings_table.php
@@ -30,6 +30,116 @@ class CreateServiceMappingsTable extends Migration
             )
         );
 
+     DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 3,
+                'category_id' => 4,
+                
+            )
+        );
+
+     DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 3,
+                'category_id' => 5,
+                
+            )
+        );
+
+        DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 1,
+                'category_id' => 13,
+                
+            )
+        );
+
+        DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 1,
+                'category_id' => 14,
+                
+            )
+        );
+
+        DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 4,
+                'category_id' => 15,
+                
+            )
+        );
+
+        DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 4,
+                'category_id' => 16,
+                
+            )
+        );
+
+        DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 4,
+                'category_id' => 17,
+                
+            )
+        );
+
+            DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 5,
+                'category_id' => 18,
+                
+            )
+        );
+
+            DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 5,
+                'category_id' => 19,
+                
+            )
+        );
+
+            DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 5,
+                'category_id' => 20,
+                
+            )
+        );
+
+              DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 2,
+                'category_id' => 10,
+                
+            )
+        );
+            DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 2,
+                'category_id' => 22,
+                
+            )
+        );
+
+          DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 17,
+                'category_id' => 23,
+                
+            )
+        );
+
+            DB::table('service_mappings')->insert(
+            array(
+                'service_id' => 17,
+                'category_id' => 24,
+                
+            )
+        );
     }
 
     /**
