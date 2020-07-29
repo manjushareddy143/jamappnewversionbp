c3c83a70b3b026ccdef6eb6e236d48e293c5b35a
Mayur Khuman <mk@savitriya.com>
2020-07-29 17:28:24 +0530

    arabic address issue

--- resources/views/invoice/invoice.blade.php
+++ resources/views/invoice/invoice.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="ar" >
+<html xmlns="http://www.w3.org/1999/xhtml">
    <head>
       <meta http-equiv='content-type' content='text/html; charset=utf-8" />
 
@@ -114,7 +114,6 @@
         html, body {
             width: 210mm;
             height: 297mm;
-            font-family: DejaVu Sans, sans-serif;
         }
     }
 
