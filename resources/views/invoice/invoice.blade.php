e8ca453e4c3e60f9aa9a3c6dbed44bbe780fb0a2
Mayur Khuman <mk@savitriya.com>
2020-07-29 16:40:58 +0530

    arabic address issue

--- resources/views/invoice/invoice.blade.php
+++ resources/views/invoice/invoice.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl" lang="ar">
    <head>
-      <meta http-equiv='content-type' content='text/html; charset=utf-8' />
+      <meta http-equiv='content-type' content='text/html; charset=utf-8' charset="utf-8" />
 
       <title>INVOICE</title>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
