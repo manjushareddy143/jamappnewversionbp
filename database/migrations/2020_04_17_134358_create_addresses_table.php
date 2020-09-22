f5b3bd58ae333f1160376236dd4ac1aa3994b278
Mayur Khuman <mk@savitriya.com>
2020-09-22 17:51:09 +0530

    update DB

--- database/migrations/2020_04_17_134358_create_addresses_table.php
+++ database/migrations/2020_04_17_134358_create_addresses_table.php
@@ -16,17 +16,34 @@ class CreateAddressesTable extends Migration
         Schema::create('addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('address_line1');
+            $table->string('address_line1')->nullable(true);
             $table->string('address_line2')->nullable(true);
             $table->string('landmark')->nullable(true);
-            $table->string('district');
-            $table->string('city');
-            $table->string('postal_code');
+            $table->string('district')->nullable(true);
+            $table->string('city')->nullable(true);
+            $table->string('postal_code')->nullable(true);
             $table->string('location')->nullable(true);
             $table->integer('user_id');
             $table->boolean('is_deleted')->default(0);
             $table->timestamps();
         });
+
+        DB::table('addresses')->insert(
+            array(
+                'name' => 'JAM office',
+                'address_line1' =>  'Sheikh Saoud Building',
+                'address_line2'=> 'Hamad Medical City',
+                'landmark' => '',
+                'district' => 'Ponomaryovsky District',
+                'city' => 'Doha',
+                'postal_code' => '461784',
+                'location' => '25.283082024872016,51.536083705723286',
+                'user_id' => 3,
+                'default_address' => 1,
+                'is_deleted' => 0,
+            )
+        );
+
     }
 
     /**
