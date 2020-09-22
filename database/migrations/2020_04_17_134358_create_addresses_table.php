228f6eef9594fd8f0a3525dd06de0da66a171053
Mayur Khuman <mk@savitriya.com>
2020-09-22 17:58:34 +0530

    reset update db

--- database/migrations/2020_04_17_134358_create_addresses_table.php
+++ database/migrations/2020_04_17_134358_create_addresses_table.php
@@ -13,36 +13,24 @@ class CreateAddressesTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('addresses');
         Schema::create('addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('address_line1')->nullable();
-            $table->string('address_line2')->nullable();
-            $table->string('landmark')->nullable();
-            $table->string('district')->nullable();
-            $table->string('city')->nullable();
-            $table->string('postal_code')->nullable();
-            $table->string('location')->nullable();
+            $table->string('address_line1')->nullable(true);
+            $table->string('address_line2')->nullable(true);
+            $table->string('landmark')->nullable(true);
+            $table->string('district')->nullable(true);
+            $table->string('city')->nullable(true);
+            $table->string('postal_code')->nullable(true);
+            $table->string('location')->nullable(true);
             $table->integer('user_id');
+            $table->integer('default_address')->default(0)->nullable();
             $table->boolean('is_deleted')->default(0);
             $table->timestamps();
         });
 
-        DB::table('addresses')->insert(
-            array(
-                'name' => 'JAM office',
-                'address_line1' =>  'Sheikh Saoud Building',
-                'address_line2'=> 'Hamad Medical City',
-                'landmark' => '',
-                'district' => 'Ponomaryovsky District',
-                'city' => 'Doha',
-                'postal_code' => '461784',
-                'location' => '25.283082024872016,51.536083705723286',
-                'user_id' => 3,
-                'default_address' => 1,
-                'is_deleted' => 0,
-            )
-        );
+
 
     }
 
--- database/migrations/2020_09_16_081104_add_default_addrtess_flag.php
+++ database/migrations/2020_09_16_081104_add_default_addrtess_flag.php
@@ -13,10 +13,26 @@ class AddDefaultAddrtessFlag extends Migration
      */
     public function up()
     {
-        Schema::table('addresses', function (Blueprint $table) {
-            $table->integer('default_address')->after('user_id')->default(0)->nullable();
-            $table->index("default_address");
-        });
+        // Schema::table('addresses', function (Blueprint $table) {
+        //     $table->integer('default_address')->after('user_id')->default(0)->nullable();
+        //     $table->index("default_address");
+        // });
+
+        DB::table('addresses')->insert(
+            array(
+                'name' => 'JAM office',
+                'address_line1' =>  'Sheikh Saoud Building',
+                'address_line2'=> 'Hamad Medical City',
+                'landmark' => '',
+                'district' => 'Ponomaryovsky District',
+                'city' => 'Doha',
+                'postal_code' => '461784',
+                'location' => '25.283082024872016,51.536083705723286',
+                'user_id' => 3,
+                'default_address' => 1,
+                'is_deleted' => 0,
+            )
+        );
 
     }
 
